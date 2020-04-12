0300c1140b6222780e63c1eb2706a953d47b098a
NYCDOE <admin@AFVFWQ0UZJC9H.fios-router.home>
2020-04-12 02:39:28 -0400

    modified gameObject details.

--- Assets/scripts/DisplayScript.cs
+++ Assets/scripts/DisplayScript.cs
@@ -5,7 +5,7 @@ using Vuforia;
 
 public class DisplayScript : MonoBehaviour, IVirtualButtonEventHandler
 {
-    public GameObject A1, A2, A3, A4, A1vid, A2vid, A3vid, A4vid;
+    public GameObject internetSlide, connectionSlide, IP_Slide, routingSlide, internetSlidevid, connectionSlidevid, IP_Slidevid, routingSlidevid;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +16,14 @@ public class DisplayScript : MonoBehaviour, IVirtualButtonEventHandler
             vrb[i].RegisterEventHandler(this);
         }
 
-        A1.SetActive(false);
-        A2.SetActive(false);
-        A3.SetActive(false);
-        A4.SetActive(false);
-        A1vid.SetActive(false);
-        A2vid.SetActive(false);
-        A3vid.SetActive(false);
-        A4vid.SetActive(false);
+        internetSlide.SetActive(false);
+        connectionSlide.SetActive(false);
+        IP_Slide.SetActive(false);
+        routingSlide.SetActive(false);
+        internetSlidevid.SetActive(false);
+        connectionSlidevid.SetActive(false);
+        IP_Slidevid.SetActive(false);
+        routingSlidevid.SetActive(false);
 
     }
 
@@ -38,50 +38,50 @@ public class DisplayScript : MonoBehaviour, IVirtualButtonEventHandler
         if (vb.VirtualButtonName == "vb1")
         {
             Debug.Log("first button pressed");
-            A1.SetActive(true);
-            A2.SetActive(false);
-            A3.SetActive(false);
-            A4.SetActive(false);
-            A1vid.SetActive(true);
-            A2vid.SetActive(false);
-            A3vid.SetActive(false);
-            A4vid.SetActive(false);
+            internetSlide.SetActive(true);
+            connectionSlide.SetActive(false);
+            IP_Slide.SetActive(false);
+            routingSlide.SetActive(false);
+            internetSlidevid.SetActive(true);
+            connectionSlidevid.SetActive(false);
+            IP_Slidevid.SetActive(false);
+            routingSlidevid.SetActive(false);
         }
         else if (vb.VirtualButtonName == "vb2")
         {
             Debug.Log("second button pressed");
-            A1.SetActive(false);
-            A2.SetActive(true);
-            A3.SetActive(false);
-            A4.SetActive(false);
-            A1vid.SetActive(false);
-            A2vid.SetActive(true);
-            A3vid.SetActive(false);
-            A4vid.SetActive(false);
+            internetSlide.SetActive(false);
+            connectionSlide.SetActive(true);
+            IP_Slide.SetActive(false);
+            routingSlide.SetActive(false);
+            internetSlidevid.SetActive(false);
+            connectionSlidevid.SetActive(true);
+            IP_Slidevid.SetActive(false);
+            routingSlidevid.SetActive(false);
         }
         else if (vb.VirtualButtonName == "vb3")
         {
             Debug.Log("third button pressed");
-            A1.SetActive(false);
-            A2.SetActive(false);
-            A3.SetActive(true);
-            A4.SetActive(false);
-            A1vid.SetActive(false);
-            A2vid.SetActive(false);
-            A3vid.SetActive(true);
-            A4vid.SetActive(false);
+            internetSlide.SetActive(false);
+            connectionSlide.SetActive(false);
+            IP_Slide.SetActive(true);
+            routingSlide.SetActive(false);
+            internetSlidevid.SetActive(false);
+            connectionSlidevid.SetActive(false);
+            IP_Slidevid.SetActive(true);
+            routingSlidevid.SetActive(false);
         }
         else if (vb.VirtualButtonName == "vb4")
         {
             Debug.Log("Fourth button pressed");
-            A1.SetActive(false);
-            A2.SetActive(false);
-            A3.SetActive(false);
-            A4.SetActive(true);
-            A1vid.SetActive(false);
-            A2vid.SetActive(false);
-            A3vid.SetActive(false);
-            A4vid.SetActive(true);
+            internetSlide.SetActive(false);
+            connectionSlide.SetActive(false);
+            IP_Slide.SetActive(false);
+            routingSlide.SetActive(true);
+            internetSlidevid.SetActive(false);
+            connectionSlidevid.SetActive(false);
+            IP_Slidevid.SetActive(false);
+            routingSlidevid.SetActive(true);
         }
         else
         {
